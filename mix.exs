e75468bee089f1efeab8e545876c5fbe52e3f737
devstopfix <devstopfix@gmail.com>
2019-03-26 10:09:44 +0000

    inch-ci trial

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule Gnuplot.MixProject do
       {:credo, "~> 1.0.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev},
+      {:inch_ex, only: :docs},
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
     ]
   end
