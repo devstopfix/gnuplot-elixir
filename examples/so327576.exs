3a8b8d9968995dd6524bed2141856c4be2357bc0
devstopfix <devstopfix@gmail.com>
2019-03-28 10:45:21 +0000

    Bar chart with labels example from SO 327576

---
+++ examples/so327576.exs
@@ -0,0 +1,23 @@
+defmodule BarChart do
+  import Gnuplot
+
+  @moduledoc "Chart from https://stackoverflow.com/a/11551808/3366"
+
+  def run do
+
+    chart = [
+      [:set, :term, :png, :size, '512,512'],
+      [:set, :output, Path.join("/tmp", "barchart.PNG")],
+      [:set, :boxwidth, 0.5],
+      ~w(set style fill solid)a,
+      [:plot, "-", :using, '1:3:xtic(2)', :with, :boxes]
+    ]
+
+    dataset = [[0, "label", 100], [1, "label2", 450], [2, "bar_label", 75]]
+
+    plot(chart, [dataset])
+  end
+end
+
+# mix run examples/so327576.exs
+BarChart.run()
