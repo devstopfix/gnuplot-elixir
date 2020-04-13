1edba2e9d431d3e9b4dfbe5964eb6ebd9a44ace0
devstopfix <devstopfix@gmail.com>
2020-04-13 16:04:21 +0100

    Elixir 1.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gnuplot.MixProject do
   def project do
     [
       app: :gnuplot,
-      version: "1.19.96",
+      version: "1.19.97",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Interface between Elixir and Gnuplot graphing library",
@@ -27,10 +27,9 @@ defmodule Gnuplot.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.0.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.5", only: [:dev], runtime: false},
-      {:ex_doc, ">= 0.19.0", only: :dev},
-      {:mix_test_watch, "~> 0.9", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.21", only: :dev}
     ]
   end
 
