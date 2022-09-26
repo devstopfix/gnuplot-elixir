3dca3058688f33ed9359e219d060a98892289202
James Every <devstopfix@gmail.com>
2022-09-26 13:28:16 +0100

    Prefer map_join (#27)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gnuplot.MixProject do
   def project do
     [
       app: :gnuplot,
-      version: "1.22.266",
+      version: "1.22.267",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       description: "Interface between Elixir and Gnuplot graphing library",
